de9cfeeb0c0f61367639f9b6bbfd7638a42d608e
Logan Ford <loganford17@gmail.com>
2016-11-30 00:56:43 -0600

    Error resolved: server was listening to the wrong port

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var app = express()
 
 app.use('/untpa', express.static(__dirname + '/public'));
 
-// app.get('/', function (req, res) {
-//   res.redirect('/untpa')
-// })
+app.get('/', function (req, res) {
+  res.redirect('/untpa')
+})
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
