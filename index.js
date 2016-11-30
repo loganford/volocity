1feb8e6241155cfce4694f96c7af5274acab6c24
Logan Ford <loganford17@gmail.com>
2016-11-30 00:51:34 -0600

    Attempting to resolve app crash on launch

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var app = express()
 
 app.use('/untpa', express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.redirect('/untpa')
-})
+// app.get('/', function (req, res) {
+//   res.redirect('/untpa')
+// })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
