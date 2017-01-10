961bd9c3bd772e0a1735c2c9cfc870a96b505ade
Logan Ford <loganford17@gmail.com>
2017-01-09 21:37:27 -0600

    Keeping admin controller DRY, added handlers for preferences / settings

--- public/js/controllers/admin.controller.js
+++ public/js/controllers/admin.controller.js
@@ -11,6 +11,7 @@ function adminController(moment, _, adminService, $scope, $location, $anchorScro
     ac.email = $scope.$parent.email;
     ac.password = $scope.$parent.password;
     ac.org = $scope.$parent.org;
+    ac.preferences = ac.org.preferences;
     ac.vol = $scope.$parent.vol;
     ac.err = 0;
     ac.tableDates = [];
@@ -33,21 +34,14 @@ function adminController(moment, _, adminService, $scope, $location, $anchorScro
         if(validEmail(ac.newVolunteer)) {
             ac.invalidNewEmail = false;
             $('#newVolModal').modal('hide');
-            ac.err = 0;
-            ac.message = {};
-            ac.working = true;
+            prepareForRequest();
             adminService.addVolunteer(ac.password, ac.org.name, ac.newVolunteer).then(function (resp) {
-                ac.err = 0;
-                ac.message.title = 'Success!';
                 ac.message.detail = 'Added ' + ac.newVolunteer + ' to ' + ac.org.name +'.';
                 ac.org = resp.data[0];
-                console.log(resp);
-                ac.working = false;
                 ac.newVolunteer = '';
+                handleResp(resp);
             }, function (err) {
-                ac.err = err.status;
-                console.log(err);
-                ac.working = false;
+                handleErr(err);
             });
         } else {
             ac.invalidNewEmail = true;
@@ -56,41 +50,27 @@ function adminController(moment, _, adminService, $scope, $location, $anchorScro
 
     ac.removeVolunteer = function() {
         $('#removeVolModal').modal('hide');
-        ac.working = true;
-        ac.err = 0;
-        ac.message = {};
+        prepareForRequest()
         adminService.removeVolunteer(ac.password, ac.org.name, ac.volToRemove).then(function (resp){
-            ac.message.title = 'Success!';
             ac.message.detail = 'Removed ' + ac.volToRemove + ' from ' + ac.org.name + '.';
             ac.org = resp.data[0];
-            console.log(resp);
-            ac.working = false;
+            handleResp(resp);
         }, function (err){
-            ac.err = err.status;
-            console.log(err);
-            ac.working = false;
+            handleErr(err);
         });
     }
 
     ac.addEvent = function(){
         if(moment(ac.newEvent).isValid()) {
-            ac.invalidNewEmail = false;
             $('#newEventModal').modal('hide');
-            ac.err = 0;
-            ac.message = {};
-            ac.working = true;
+            prepareForRequest();
             adminService.addEvent(ac.password, ac.org.name, ac.newEvent).then(function (resp) {
-                ac.err = 0;
-                ac.message.title = 'Success!';
                 ac.message.detail = 'Added ' + moment(ac.newEvent).format('MMMM D, YYYY') + ' to ' + ac.org.name +'.';
                 ac.org = resp.data[0];
-                console.log(resp);
-                ac.working = false;
+                handleResp(resp);
                 updateTableDates();
             }, function (err) {
-                ac.err = err.status;
-                console.log(err);
-                ac.working = false;
+                handleErr(err);
             });
         } else {
             ac.invalidDate = true;
@@ -99,20 +79,82 @@ function adminController(moment, _, adminService, $scope, $location, $anchorScro
 
     ac.removeEvent = function() {
         $('#removeEventModal').modal('hide');
-        ac.working = true;
-        ac.err = 0;
-        ac.message = {};
+        prepareForRequest();
         adminService.removeEvent(ac.password, ac.org.name, ac.eventToRemove).then(function (resp){
-            ac.message.title = 'Success!';
             ac.message.detail = 'Removed ' + moment(ac.eventToRemove).format('MMMM D, YYYY') + ' from events.';
             ac.org = resp.data[0];
-            console.log(resp);
-            ac.working = false;
+            handleResp(resp);
             updateTableDates();
         }, function (err){
-            ac.err = err.status;
-            console.log(err);
-            ac.working = false;
+            handleErr(err);
+        });
+    }
+
+    ac.changeAdminEmail = function(){
+        if (validEmail(ac.newAdminEmail)) {
+            $('#adminEmailModal').modal('hide');
+            prepareForRequest()
+            ac.invalidEmail = false;
+            adminService.updateAdminEmail(ac.password, ac.org.name, ac.newAdminEmail).then(function(resp){
+                ac.message.detail = 'The admin e-mail has been changed to ' + ac.newAdminEmail + '.';
+                handleResp(resp);
+            }, function (err){
+                handleErr(err);
+            })
+        } else {
+            ac.invalidEmail = true;
+        }
+    }
+
+    ac.changeAdminPassword = function(){
+        ac.invalidPassword = false;
+        ac.invalidConfirmPassword = false;
+        if (ac.newAdminPassword != '' && ac.newAdminPassword != ac.password) {
+            if (ac.newAdminPassword == ac.confirmNewAdminPassword) {
+                $('#adminPasswordModal').modal('hide');
+                prepareForRequest()
+                adminService.updatePassword(ac.password, ac.org.name, ac.newAdminPassword, 'Admin').then(function (resp) {
+                    ac.message.detail = 'The admin password has been successfully updated.';
+                    handleResp(resp);
+                }, function (err) {
+                    handleErr(err);
+                })
+            } else {
+                ac.invalidConfirmPassword = true;
+            }
+        } else {
+            ac.invalidPassword = true;
+        }
+    }
+
+    ac.changeVolPassword = function(){
+        ac.invalidPassword = false;
+        ac.invalidConfirmPassword = false;
+        if (ac.newVolPassword != '') {
+            if (ac.newVolPassword == ac.confirmNewVolPassword) {
+                $('#volPasswordModal').modal('hide');
+                prepareForRequest()
+                adminService.updatePassword(ac.password, ac.org.name, ac.newVolPassword, 'Volunteer').then(function (resp) {
+                    ac.message.detail = 'The volunteer password has been successfully updated.';
+                    handleResp(resp);
+                }, function (err) {
+                    handleErr(err);
+                })
+            } else {
+                ac.invalidConfirmPassword = true;
+            }
+        } else {
+            ac.invalidPassword = true;
+        }
+    }
+
+    ac.updatePreference = function(){
+        prepareForRequest();
+        adminService.updatePreference(ac.password, ac.org.name, 'autoEmail', ac.autoEmail).then(function(resp){
+            handleResp(resp);
+            ac.message.detail = 'Your preferences have been updated.';
+        }, function(err){
+           handleErr(err);
         });
     }
 
@@ -123,6 +165,25 @@ function adminController(moment, _, adminService, $scope, $location, $anchorScro
 
     ac.getCountdown = function(e) {
         return moment(e.date).diff(moment(), 'days');
+    }
+
+    function prepareForRequest() {
+        ac.working = true;
+        ac.err = 0;
+        ac.message = {};
+    }
+
+    function handleResp(resp){
+        ac.message.title = 'Success!';
+        console.log(resp);
+        ac.working = false;
+        ac.err = 0;
+    }
+
+    function handleErr(err){
+        console.log(err);
+        ac.working = false;
+        ac.err = err.status;
     }
 
     function updateTableDates(){
