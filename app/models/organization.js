e6f60e2aae8f3023709ce4fc155c3e637949852e
Logan Ford <loganford17@gmail.com>
2017-01-09 21:36:45 -0600

    Updated organization model

--- app/models/organization.js
+++ app/models/organization.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Organization', {
     name : String,
     admin: String,
+    preferences: Object,
     events: Array,
     volunteers: Array
 });
